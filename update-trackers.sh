e326c13fd1ee5c7734a41cca9a97e7c47ea1cc8e
urain39 <urain39@qq.com>
2019-11-03 10:07:16 +0800

    trackers: add nyaa default trackers.

--- update-trackers.sh
+++ update-trackers.sh
@@ -2,7 +2,10 @@
 
 TRACKER_LIST_BEST="https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt"
 TRACKER_LIST_BEST_IP="https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt"
+TRACKER_LIST_NYAA="https://raw.githubusercontent.com/nyaadevs/nyaa/master/trackers.txt"
+
 
 wget -O - "${TRACKER_LIST_BEST}" > trackers.txt
 wget -O - "${TRACKER_LIST_BEST_IP}" >> trackers.txt
+wget -O - "${TRACKER_LIST_NYAA}" >> trackers.txt
 
