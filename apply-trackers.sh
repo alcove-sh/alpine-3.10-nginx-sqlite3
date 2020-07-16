274ad70d0d53509529ad36831b4c505b34b20e18
urain39 <16981964+urain39@users.noreply.github.com>
2020-07-16 18:11:49 +0800

    Update apply-trackers.sh

--- apply-trackers.sh
+++ apply-trackers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TRACKER_LIST=`sed -n '/^#/d; /^ *$/d; p' trackers.txt | tr '\n' ','`
+TRACKER_LIST=`sed -n '/^#/d; /^[\t ]*$/d; p' trackers.txt | tr '\n' ','`
 
 sed -i "s|^\(bt-tracker\)=.*|\1=${TRACKER_LIST}|g" "aria2.conf.example"
 
